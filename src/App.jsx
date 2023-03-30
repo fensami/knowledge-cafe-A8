a5d9d69af33d95ee938c19fd228faf7c4f679381
fensami <113832498+fensami@users.noreply.github.com>
2023-03-30 20:43:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello assignment 8 kaku</h1>
+
+    </div>
+  )
+}
+
+export default App
